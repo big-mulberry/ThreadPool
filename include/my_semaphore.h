2d4f09b90a68223eb4a12f41220af8ef99b54e84
sjl <1402508670@qq.com>
2025-01-05 16:10:15 +0800

    fix: 解决条件变量析构后的wait未定义问题

--- include/my_semaphore.h
+++ include/my_semaphore.h
@@ -7,7 +7,12 @@ class MySemaphore
 {
   public:
     MySemaphore(int resource_limit = 0);
-    ~MySemaphore() = default;
+
+    // 注意析构函数
+    // result指针会传给task
+    // result对象析构时，task对象还在，并在另一线程中调用result的setValue方法
+    // 伴随result对象析构的MySemaphore中的条件变量的wait和notify会导致未定义的结果
+    ~MySemaphore();
 
     //* 获取信号量资源
     void wait();
@@ -19,5 +24,6 @@ class MySemaphore
     int resourceLimit_;
     std::mutex mtx_;
     std::condition_variable cv_;
+    bool isRunning_;
 };
 } // namespace ThreadPool
--- src/my_semaphore.cpp
+++ src/my_semaphore.cpp
@@ -2,17 +2,30 @@
 
 namespace ThreadPool
 {
-MySemaphore::MySemaphore(int resource_limit) : resourceLimit_(resource_limit)
+MySemaphore::MySemaphore(int resource_limit) : resourceLimit_(resource_limit), isRunning_(true)
 {
+}
+MySemaphore::~MySemaphore()
+{
+    isRunning_ = false;
 }
 void MySemaphore::wait()
 {
+    if (!isRunning_)
+    {
+        // result对象已经析构，不再在意结果
+        return;
+    }
     std::unique_lock<std::mutex> locker(mtx_);
     cv_.wait(locker, [this]() -> bool { return resourceLimit_ > 0; });
     resourceLimit_--;
 }
 void MySemaphore::post()
 {
+    if (!isRunning_)
+    {
+        return;
+    }
     std::unique_lock<std::mutex> locker(mtx_);
     resourceLimit_++;
     cv_.notify_all();
